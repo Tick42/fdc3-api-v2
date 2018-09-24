340bbb3181391351c0121ce24c08c217ef78fea6
Leslie Spiro <lspiro@Leslies-MacBook-Pro.local>
2018-09-24 17:04:28 +0100

    WIP

--- src/interface.ts
+++ src/interface.ts
@@ -33,7 +33,7 @@ enum ResolveError {
 }
 
 /**
- * An fdc3Access instance provides an application with access to the FDC3 services from one
+ * An fdc3Access instance provides an application with access to the FDC3 services provided by one
  * or more platforms.
  * Usage Note. For applications running on a Platform, the Platform may provide a pre-initialised 
  * fdc3Access instance. Applications may also instantiate their own instances which connect directly 
@@ -71,8 +71,9 @@ interface fdc3Access {
      * TODO: Retrun a launch object which can provide launch errors and also optional access to 
      * an applicationInstanceId to identify an application instance.
      * TODO: Check on app name vs app id in AppD structure.
+     * Default platform ?
      */
-    openApplication(name: String, context?: Context, config?: String): Promise<ApplicationInstance>;
+    openApplication(app: string|Application, context?: Context, config?: string, platform?: string|Platform): Promise<ApplicationInstance>;
   
     /**
      * List the running instances of an Application.
@@ -91,11 +92,6 @@ interface fdc3Access {
      */
     activateAppInstance( instance: ApplicationInstance );     
     
-    /**
-     * Provide a list of 'read only' info on the platforms that this instance of fdc3Access is providing access to.
-     */
-    listAppDirectories(): Promise<ApplicationDirectoryServer[]>;
-
 
     // Intents -----------------
   
@@ -107,7 +103,7 @@ interface fdc3Access {
    * @param context : An optional Context that is used to filter the Intents. 
    *                  
    */
-    resolveByIntent(intent: IntentName, context?: Context): Promise<AppIntent[]>;
+    resolveByIntent(intent: IntentName, context?: Context): Promise<Intent[]>;
 
     /**
      * Return the Intents available that can implement an action for the given Context.
@@ -125,7 +121,7 @@ interface fdc3Access {
     /**
      * Raises an intent to the desktop agent to resolve.
      */
-    raiseIntent(intent: IntentName, context: Context, target?: ApplicationInstance): Promise<IntentResolution>;
+    raiseIntent(intent: IntentName|Intent, context: Context): Promise<IntentResolution>;
   
     /**
      * Listens to incoming Intents from the Platforms.
@@ -174,13 +170,23 @@ interface Platform {
   platformApi?: object; // optional accessor to the Platform interop API, for example the Platform Agnostic interop API
 }
 
+/**
+ * An FDC3 Application. 
+ * UsageNote: Typically FDC3 applications are defined in an Applicastion Directory and started via FDC3
+ * However applications can also be started 'outside' FDC3 and announce themselves. 
+ */
+interface Application {
+  appId: string;  // FDC App D unique id. or missing if empty or null then this is not an App Directory defined application
+  name?: string;  // App name
+  platformName: string;  // If the application runs on a Platform, the name of the Platform. 
+  appType?: string; // FDC3 AppD application type.
+}
+
 interface ApplicationInstance {
-    appId?: string;  // FDC App D unique id. or missing if not an App Directory defined application
-    name: string;
+    app: Application;
     instanceId: string; // some id tbat uniquely indicateds whihc instance of the app where multiple instances are valid. 
                         // Blank if single isntance app.
-    appType?: string;
-    platform?: string;  // If appType is "Platform" the name of the Platform hosting the application.
+
 }
 
 /**
@@ -219,13 +225,6 @@ interface Intent {
    * If the resolution errors, it returns an `Error` with a string from the `ResolveError` enumeration.
    */
   send(context: Context, target?: AppIdentifier): Promise<void>
-}
-
-/**
- * App metadata is Desktop Agent specific - but should support a name property.
- */
-interface AppMetadata {
-  name: AppIdentifier;
 }
 
 /**
